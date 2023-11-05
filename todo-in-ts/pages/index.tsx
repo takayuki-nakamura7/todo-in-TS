e434650975820e47913798aad313cc1fc5df4a03
takayuki nakamura <ktkr7195@gmai.com>
2023-11-05 14:57:42 -0500

    Add addTodo input

--- todo-in-ts/pages/index.tsx
+++ todo-in-ts/pages/index.tsx
@@ -4,6 +4,19 @@ import { Todo } from '../types/types'; // Assuming you have defined this type
 
 const Home: React.FC = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [inputValue, setInputValue] = useState(''); 
+
+
+  const addTodo = () => {
+    if (!inputValue.trim()) return;
+    const newTodo: Todo = {
+      id: Math.random(),
+      text: inputValue,
+      completed: false,
+    };
+    setTodos([...todos, newTodo]);
+    setInputValue('');
+  }
 
   const deleteTodo = (id: number) => {
     setTodos(todos.filter(todo => todo.id !== id));
@@ -14,6 +27,13 @@ const Home: React.FC = () => {
   return (
     <div>
       <h1>My ToDo App</h1>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        placeholder="Add a new todo..."
+      />
+      <button onClick={addTodo}>Add Todo</button>
       <TodoList todos={todos} deleteTodo={deleteTodo} />
       {/* You will also need to render components or buttons to add/edit todos */}
     </div>
