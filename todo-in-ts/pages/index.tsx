c0198202eaeea5ce013850347a09c15f0b9e6786
takayuki nakamura <ktkr7195@gmai.com>
2023-11-05 14:05:01 -0500

    Update index.tsx

--- todo-in-ts/pages/index.tsx
+++ todo-in-ts/pages/index.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
-import TodoList from '../components/TodoList';
+import React, { useState } from 'react';
+import { TodoList } from '../components/TodoList';
+import { Todo } from '../types/types'; // Assuming you have defined this type
 
 const Home: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const deleteTodo = (id: number) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
+  // ... other handlers like addTodo, editTodo, etc.
+
   return (
     <div>
       <h1>My ToDo App</h1>
-      <TodoList />
+      <TodoList todos={todos} deleteTodo={deleteTodo} />
+      {/* You will also need to render components or buttons to add/edit todos */}
     </div>
   );
 };
