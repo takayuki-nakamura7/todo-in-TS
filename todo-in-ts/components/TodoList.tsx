22dd470b1eb66c3a3193679e5903dcea64423b5c
takayuki nakamura <ktkr7195@gmai.com>
2023-11-05 13:24:57 -0500

    Update list component

--- todo-in-ts/components/TodoList.tsx
+++ todo-in-ts/components/TodoList.tsx
@@ -1,52 +1,18 @@
-import React, { useState } from 'react';
+import { TodoItem } from './TodoItem';
+import { Todo } from '../types/types';
 
-// Define the shape of a todo item using TypeScript interfaces
-interface Todo {
-  id: number;
-  text: string;
-  completed: boolean;
+interface TodoListProps {
+  todos: Todo[];
+  deleteTodo: (id: number) => void;
+  // pass other handlers like editTodo as props
 }
 
-const TodoList: React.FC = () => {
-  // State to hold the array of todos
-  const [todos, setTodos] = useState<Todo[]>([]);
-
-  // Function to add new todo to the state
-  const addTodo = (todoText: string) => {
-    const newTodo: Todo = { id: Date.now(), text: todoText, completed: false };
-    setTodos([...todos, newTodo]);
-  };
-
-  // Function to delete a todo from the state
-  const deleteTodo = (todoId: number) => {
-    setTodos(todos.filter((todo) => todo.id !== todoId));
-  };
-
-  // Function to toggle the completed status of a todo item
-  const toggleTodoCompletion = (todoId: number) => {
-    setTodos(
-      todos.map((todo) =>
-        todo.id === todoId ? { ...todo, completed: !todo.completed } : todo
-      )
-    );
-  };
-
-  // The component's UI
+export const TodoList: React.FC<TodoListProps> = ({ todos, deleteTodo }) => {
   return (
     <div>
-      {/* Render your todos here */}
       {todos.map((todo) => (
-        <div key={todo.id}>
-          <span style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
-            {todo.text}
-          </span>
-          <button onClick={() => toggleTodoCompletion(todo.id)}>Toggle</button>
-          <button onClick={() => deleteTodo(todo.id)}>Delete</button>
-        </div>
+        <TodoItem key={todo.id} todo={todo} deleteTodo={deleteTodo} />
       ))}
-      {/* Here, you'll need a way for the user to input a todo and add it using the addTodo function */}
     </div>
   );
 };
-
-export default TodoList;
