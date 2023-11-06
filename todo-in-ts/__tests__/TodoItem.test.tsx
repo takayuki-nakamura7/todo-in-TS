1f48194e5dd1511637c9eaa33594ddda21b60578
takayuki nakamura <ktkr7195@gmai.com>
2023-11-05 19:00:15 -0500

    Add tests for TodoItem component with delete, edit, and completion functionality

--- todo-in-ts/__tests__/TodoItem.test.tsx
+++ todo-in-ts/__tests__/TodoItem.test.tsx
@@ -1,13 +1,41 @@
-import { render, screen } from '@testing-library/react';
-import TodoItem from '../components/TodoItem';
+import { render, screen, waitFor } from '@testing-library/react';
+import { TodoItem } from '../components/TodoItem';
 import userEvent from '@testing-library/user-event';
 
 describe('TodoItem', () => {
+  const mockDeleteTodo = jest.fn();
+  const mockEditTodo = jest.fn();
+  const mockToggleComplete = jest.fn();
+
   it('renders the todo item correctly', () => {
-    render(<TodoItem todo={{ id: '1', text: 'Test Todo', completed: false }} />);
+    render(
+      <TodoItem
+        todo={{ id: '1', text: 'Test Todo', completed: false }}
+        deleteTodo={mockDeleteTodo}
+        editTodo={mockEditTodo}
+        toggleComplete={mockToggleComplete}
+      />
+    );
 
     expect(screen.getByText('Test Todo')).toBeInTheDocument();
   });
 
-  // Add more tests to interact with the todo item...
+  it('triggers completion toggle when checkbox is clicked', async () => {
+    render(
+      <TodoItem
+        todo={{ id: '1', text: 'Test Todo', completed: false }}
+        deleteTodo={mockDeleteTodo}
+        editTodo={mockEditTodo}
+        toggleComplete={mockToggleComplete}
+      />
+    );
+
+    const checkbox = screen.getByRole('checkbox');
+    userEvent.click(checkbox);
+
+    // Wait for the mockToggleComplete function to be called after the click event
+    await waitFor(() => expect(mockToggleComplete).toHaveBeenCalledWith('1'));
+  });
+
+  // Add more tests here for delete and edit actions
 });
