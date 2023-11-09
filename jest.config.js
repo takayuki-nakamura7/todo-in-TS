ffff11dd7ec622a1fcff097b0a8988321e35856c
takayuki nakamura <ktkr7195@gmai.com>
2023-11-09 13:15:50 -0500

    Update Jest config to handle TypeScript and JavaScript files

--- jest.config.js
+++ jest.config.js
@@ -3,9 +3,8 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   transform: {
-    // Use babel-jest to transpile tests with the next/babel preset
-    // This is necessary for Next.js versions 9 and up
-    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+    // Handle TypeScript and JavaScript files
+    '^.+\\.(ts|tsx|js|jsx)$': ['babel-jest', { presets: ['next/babel'] }],
   },
-  testEnvironment: 'jest-environment-jsdom',
+  testEnvironment: 'jsdom',
 };
